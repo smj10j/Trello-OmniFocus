b06b5ed0729a8d988e76daedf52521c94011dcc0
Stephen Johnson <stephen@extrabux.com>
2013-11-15 21:45:59 -0800

    Standardize parameters

--- scripts/autodeployer.sh
+++ scripts/autodeployer.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
 
-LOCAL_DIR=src
-DEPLOY_CMD="./scripts/deploy.sh $LOCAL_DIR"
+# Options and Usage
+
+usage() { 
+	echo -e "\
+Usage: $0 [-p <path>] [-q] [-h]\n\
+Options:\n\
+	-p <path>       Path to watch and deploy from (default: src)\n\
+	-q              Quiet mode\n\
+	-h              Show this help\n" 1>&2; 
+	exit 1;
+}
+
+while getopts ":qp:" o; do
+    case "${o}" in
+        q)
+            QUIET='-q'
+            ;;
+        p)
+            DIR=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${DIR}" ]; then
+    DIR=src
+fi
 
 
-echo "Performing initial sync..."
+
+
+
+
+# Functionality starts here
+
+DEPLOY_CMD="./scripts/deploy.sh -p $DIR $QUIET"
+
+echo "Performing initial sync with DEPLOY_CMD=$DEPLOY_CMD..."
 $DEPLOY_CMD
 
 while true; do
-	echo "Watching '$LOCAL_DIR' for changes ("`date`")..."
+	if [ -z "$QUIET" ]; then echo "Watching '$DIR' for changes ("`date`")..."; fi
 	if [ -z "`which inotifywait`" ]; then
-		./lib/fswatch/fswatch $LOCAL_DIR "$DEPLOY_CMD"
+		./lib/fswatch/fswatch $DIR "$DEPLOY_CMD"
 	else
-		inotifywait $LOCAL_DIR && $DEPLOY_CMD
+		inotifywait $DIR && $DEPLOY_CMD
 	fi
 done
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
 
-if [ -z "$1" ];then
-	echo "Usage: $0 local_dir"
-	exit 1
+# Options and Usage
+
+usage() { 
+	echo -e "\
+Usage: $0 [-p <path>] [-q] [-h]\n\
+Options:\n\
+	-p <path>       Path to deploy from\n\
+	-q              Quiet mode\n\
+	-h              Show this help\n" 1>&2; 
+	exit 1;
+}
+
+while getopts ":qp:" o; do
+    case "${o}" in
+        q)
+            QUIET=1
+            ;;
+        p)
+            D=${OPTARG%*/}
+            if [[ $D == */* ]]; then 
+            	PARENT_DIR=`dirname $D`
+            	LOCAL_DIR=${D/$PARENT_DIR\/}
+            else
+	            LOCAL_DIR=$D
+				PARENT_DIR=`pwd`
+            fi
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "$LOCAL_DIR" ];then
+	usage
 fi
 
-LOCAL_DIR=$1
-WORKING_DIR=`pwd`
+
+
+# Functionality starts here
 
 REMOTE_HOST=conquerllc.com
 REMOTE_USER=root
@@ -14,11 +48,12 @@ REMOTE_WEB_DIR=/usr/share/nginx/www
 REMOTE_DIR=trello-omnifocus
 
 NOTIFIER_OPEN_URL="http://$REMOTE_HOST/$REMOTE_DIR"
-NOTIFIER_GROUP_ID="$WORKING_DIR/$LOCAL_DIR.autodeployer"
+NOTIFIER_GROUP_ID="$PARENT_DIR/$LOCAL_DIR.autodeployer"
+NOTIFIER_DIR_DISPLAY_NAME="${PARENT_DIR##*/}/$LOCAL_DIR"
 
-echo ""
-echo "------- Deployment Started at "`date`"-------"
-echo "Deploying contents of '"$LOCAL_DIR"' to '"$REMOTE_DIR"' on $REMOTE_HOST as user $REMOTE_USER"
+if [ -z "$QUIET" ]; then echo echo ""; fi
+if [ -z "$QUIET" ]; then echo echo "------- Deployment of $NOTIFIER_DIR_DISPLAY_NAME started -------"; fi
+if [ -z "$QUIET" ]; then echo echo "Deploying contents of '"$LOCAL_DIR"' to '"$REMOTE_DIR"' on $REMOTE_HOST as user $REMOTE_USER"; fi
 
 RESULTS=`rsync -av $LOCAL_DIR/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_WEB_DIR/$REMOTE_DIR/`
 TMP=${RESULTS#sending incremental file list*}
@@ -26,13 +61,13 @@ FILE_LIST=`echo "${TMP%sent*}" | tr -d ' '`
 
 if [ -n "$FILE_LIST" ]; then
 	if [ -n "`which growlnotify`" ]; then
-		growlnotify -t "Deployed ${WORKING_DIR##*/}/$LOCAL_DIR" -m "$FILE_LIST" -d "$NOTIFIER_GROUP_ID" --appIcon "Google Chrome" --url "$NOTIFIER_OPEN_URL"
+		growlnotify -t "Deployed $NOTIFIER_DIR_DISPLAY_NAME" -m "$FILE_LIST" -d "$NOTIFIER_GROUP_ID" --appIcon "Google Chrome" --url "$NOTIFIER_OPEN_URL"
 	elif [ -n "`which terminal-notifier`" ]; then
-		terminal-notifier -title "Deployed ${WORKING_DIR##*/}/$LOCAL_DIR" -subtitle "$FILE_LIST" -group "$NOTIFIER_GROUP_ID" --appIcon "Google Chrome" -open "$NOTIFIER_OPEN_URL"
+		terminal-notifier -title "Deployed $NOTIFIER_DIR_DISPLAY_NAME" -subtitle "$FILE_LIST" -group "$NOTIFIER_GROUP_ID" --appIcon "Google Chrome" -open "$NOTIFIER_OPEN_URL"
 	fi
 fi
 
-echo "------- Deployment Finished at "`date`"-------"
-echo ""
+echo "------- Deployed $NOTIFIER_DIR_DISPLAY_NAME -------"
+if [ -z "$QUIET" ]; then echo echo ""; fi
 
 
