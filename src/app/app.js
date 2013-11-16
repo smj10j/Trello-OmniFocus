fadebfb04570e163ac6c3f93b4914a3094ff046c
Stephen Johnson <stephen@extrabux.com>
2013-11-15 20:42:37 -0800

    Adding/removing boards from sync list

--- src/app/app.js
+++ src/app/app.js
@@ -2,8 +2,25 @@
 window.OFTrello = function(opts) {
 
 	/* Member Variables */
-	this.onAuthorizationChange = opts.onAuthorizationChange || function() {};
+
+	this.isAuthorized = false;
+	this.member = null;
+	this.boardsToSync = [];
+	this.outputContainer = opts.outputContainer || $("<div>").appendTo($('body'));
+
+	/* Closure debunker */
 	var me = this;
+
+	/* Options & Callback */
+	this.onAuthorizationChange = function(member) {
+		this.isAuthorized = Trello.authorized();
+		this.member = member;
+		if(opts.onAuthorizationChange) {
+			opts.onAuthorizationChange.apply(opts, arguments);
+		}
+	};
+	
+	
 	
 	
 	/* Public Methods */
@@ -36,17 +53,123 @@ window.OFTrello = function(opts) {
 	
 	this.getBoards = function() {
 	
-		var $boards = $("<div>")
-			.text("Loading Boards...")
-			.appendTo("#output");
+		// Create some containers for displaying results 
+		$syncedBoardsHeader = $("<div>")
+			.addClass("h-divider")
+			.text("Synced Boards")
+			.appendTo(this.outputContainer);
+		$syncedBoards = $("<div>")
+			.attr('id', 'available-boards')
+			.appendTo(this.outputContainer);
+				
+		$availableBoardsHeader = $("<div>")
+			.addClass("h-divider")
+			.text("Available Boards")
+			.appendTo(this.outputContainer);
+		$availableBoards = $("<div>")
+			.attr('id', 'available-boards')
+			.appendTo(this.outputContainer);	
+	
+		var onClickSyncedBoard = function() {
+			var board = $(this).data('board');
+			console.log("Removing board \"" + board.name + "\" (id: "+board.id+") from the list of boards to be synced");
+			var newBoardsToSync = [];
+			for(var i in me.boardsToSync) {
+				var aBoard = me.boardsToSync[i];
+				if(aBoard.id != board.id) {
+					newBoardsToSync.push(aBoard);
+				}
+			}
+			me.boardsToSync = newBoardsToSync;
+			window.localStorage.setItem('boardsToSync', JSON.stringify(me.boardsToSync));
+			
+			$(this).remove();
+			$("#board-"+board.id).toggle();
+		};
+		
+		var onClickAvailableBoard = function() {
+			var board = $(this).data('board');
+			console.log("Adding board \"" + board.name + "\" (id: "+board.id+") to list of boards to be synced");
+			me.boardsToSync.push(board);
+			window.localStorage.setItem('boardsToSync', JSON.stringify(me.boardsToSync));
+			
+			$(this).clone()
+				.attr('id', "synced-board-"+board.id)
+				.data('board', board)
+				.click(onClickSyncedBoard)
+				.appendTo($syncedBoards);
+			$(this).toggle();
+		};
+	
+	
+		// List out the boards that are currently being synced
+		this.boardsToSync = window.localStorage.getItem('boardsToSync');
+		this.boardsToSync = this.boardsToSync ? JSON.parse(this.boardsToSync) : [];
+		if(this.boardsToSync.length > 0) {
 
-
+			$.each(this.boardsToSync, function(ix, board) {
+				$("<div>")
+					.addClass("board")
+					.text(board.name + " (-)")
+					.data('board', board)
+					.appendTo($syncedBoards)
+					.click(onClickSyncedBoard)
+				;
+			});	
+			
+		}else {
+			$("<div>")
+				.addClass("board")
+				.text("No boards currently syncing - add some below!")
+				.appendTo($syncedBoards)
+			;
+		}
+		
+		
+		// List out the boards that are available
+		$availableBoards.text("Loading Boards...");
 		Trello.get("members/me/boards", {
 				filter: 'open'
 			},
-			function(cards) {
+			function(boards) {
+				$availableBoards.empty();
+				$.each(boards, function(ix, board) {	
+					$board = $("<div>")
+						.attr('id','board-'+board.id)
+						.addClass("board")
+						.text(board.name + " (+)")
+						.data('board', board)
+						.appendTo($availableBoards)
+						.click(onClickAvailableBoard)
+					;
+					
+					$.each(me.boardsToSync, function(ix, boardToSync) {
+						if(boardToSync.id == board.id) {
+							$board.toggle();
+						}
+					});
+				});
+			},
+			function() {
+				console.log("Failed to load boards...");
+			}  
+		);
+	};
+	
+	this.createCard = function() {
+
+		Trello.post("members/me/cards", {
+				name: 'open',
+				desc: '',
+				pos: 'bottom',
+				due: null,
+				labels: '',
+				idList: '',
+				idMembers: this.member.id
+			},
+			function(boards) {
 				$boards.empty();
-				$.each(cards, function(ix, board) {
+				$.each(boards, function(ix, board) {
 					console.log(board);
 					$("<a>")
 						.attr({href: board.url, target: "trello"})
@@ -61,38 +184,13 @@ window.OFTrello = function(opts) {
 		);
 	};
 	
-	this.getCards = function() {
-		//Trello.post("cards/" + card.id + "/actions/comments", { text: "Hello from jsfiddle.net!" })
-		// Output a list of all of the cards that the member 
-		// is assigned to
-		Trello.get("members/me/cards", {
-				filter: 'open'
-			},
-			function(cards) {
-				$cards.empty();
-				$.each(cards, function(ix, card) {
-					$("<a>")
-						.attr({href: card.url, target: "trello"})
-						.addClass("card")
-						.text(card.name)
-						.appendTo($cards);
-				});
-			},
-			function() {
-		
-			}
-		);
-	};
-
 
 	/* Private Methods */
 
 	this._onAuthorize = function() {
-		me.onAuthorizationChange();
-
 		Trello.members.get("me", function(member){
-			$("#fullName").text(member.fullName);
-			me.getBoards();			
+			me.onAuthorizationChange(member);
+			me.getBoards();	
 		});
 	};
 
